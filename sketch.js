44f776f41df9b4410b3a939b7821fd0743d88378
Sreevidya <stgsreevidya@gmail.com>
2021-01-06 14:39:01 +0530

    add

--- sketch.js
+++ sketch.js
@@ -1,15 +1,18 @@
 const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Engine = Matter.Engine;
-var ball,ground
+var ball,ground,box
 var engine,world
 function setup() {
 
   createCanvas(800,400);
   engine = Engine.create();
   world = engine.world;
+
+  box = new Box();
   var balloptions ={
-    restitution: 0.7
+    restitution: 0.7,
+    isStatic:false
   }
   ball = Bodies.circle(400,20,20,balloptions);
   World.add(world,ball);
@@ -26,7 +29,8 @@ function setup() {
 function draw() {
   background("black"); 
   Engine.update(engine);
-  fill("brown");
+  box.display();
+    fill("brown");
   rectMode(CENTER);
   rect(ground.position.x,ground.position.y,800,20);
   fill("lightgreen");
